b78919bb5688911ca35c956fd48c80084926063c
Tagesenergie-Twitterbot <tagesenergie.bot@gmail.com>
2018-04-03 16:26:10 +0200

    fixing FileNotFoundError for \logs\ path

--- bot/loggingservice.py
+++ bot/loggingservice.py
@@ -5,7 +5,7 @@ from bot import timetool
 def log(message, logfile_name):
     """Log message to logfile."""
     path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    path += "\\logs"
-    with open(os.path.join(path, logfile_name), 'a+') as f:
+
+    with open(os.path.join(os.path.join(path, "logs"), logfile_name), 'a+') as f:
         t = timetool.get_date_and_time()
         f.write( t + " " + message + "\n\n")
