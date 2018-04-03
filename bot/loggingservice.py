f7b5bd5297941bd2417bc673f9b0268de6e73fe9
Tagesenergie-Twitterbot <tagesenergie.bot@gmail.com>
2018-04-03 16:53:18 +0200

    updating logging capabilities

--- bot/loggingservice.py
+++ bot/loggingservice.py
@@ -4,8 +4,8 @@ from bot import timetool
 
 def log(message, logfile_name):
     """Log message to logfile."""
-    path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    path = os.path.realpath((os.path.join(os.getcwd(), os.path.abspath(__file__ + "/../../"), "logs")))
 
-    with open(os.path.join(os.path.join(path, "logs"), logfile_name), 'a+') as f:
+    with open(os.path.join(path, logfile_name), 'a+') as f:
         t = timetool.get_date_and_time()
-        f.write( t + " " + message + "\n\n")
+        f.write(t + " " + message + "\n\n")
